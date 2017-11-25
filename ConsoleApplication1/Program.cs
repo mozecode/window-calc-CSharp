16d5e3bf575861898a2fc670f09198dd5c7d2068
Greta Moseley <greta.moseley@gmail.com>
2017-11-25 14:22:47 -0600

    "add window exercise with constructors, helper functions, getters and setters"

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //build windows
+
+            Window w1 = new Window();
+            //Window is data type, making spot in memory to hold a window, but it has no data inside it yet
+            //to assign data, we have to build the window using - new  and choosing one of the constructors we built by calling --Window();
+            //this window is w1
+
+            w1.Efficient = true; //changing window 1's efficiency value  and overwrites original data
+            w1.Length = -9000;  //doing same for length
+
+            Window w2 = new Window(true);
+
+            //creates a second window
+            //uses constructor that just takes boolean value (back in window.cs --public Window (bool isEfficient)
+
+            Window w3 = new Window(10, 10);
+
+            //uses constructor in Window.cs that takes two perameters --public Window (int length, int width)
+
+            Window w4 = new Window(26, 28, true);
+
+            //uses constructor in Window.cs that takes 3 perameters
+
+            double tempPrice = w3.GetPrice(); //tempPrice receives price from class
+            Console.WriteLine("The price of Window 3 is {0:C}", tempPrice);
+
+            //lets start a windows quote app here
+
+            //see class notes
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/Window.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Window
+    {
+
+        //data members
+        //attributes or characteristics of window
+        //only accessible inside class unless you add properties to access data
+        //want to control against bad input data -- that's why it's private
+
+        //****do initialization inside a constructor, not here.****
+
+        private int length;
+        private int width;
+        private bool efficient;
+        //bool holds true/false data (could also use string)
+
+
+
+        //public constructors
+        //methods called when we build an instance of this window to initialize our window
+        //has no return type, must have same name as class, just capitalized
+
+        public Window()  //no perameters = default constructor-- generic starting point if user enters no alternative
+        {
+            length = 24;  //single = sign is assigning value, unlike == comparison value
+            width = 12;
+            efficient = false;
+
+        }
+
+        public Window(bool isEfficient)  //perameterized constructor (has user entered values here it needs to use to do the work)
+        {
+            length = 24;  //single = sign is assigning value, unlike == comparison value
+            width = 12;
+            efficient = isEfficient;
+
+        }
+
+        public Window(int length, int width)  //building a bunch of shortcuts to do the work
+        {
+            //this.private data member= input perameter passed in from parentheses;
+            if (length >= 4)
+            { this.length = length; }
+
+            else { this.length = 24;  //give a default value
+            }
+            Width = width;  //go through property to protect -- it defaults back to original values
+            efficient = false;
+
+
+        }
+
+        public Window (int length, int width, bool eff) //can use other names in parentheses for what user passes to you, 
+        {
+
+            this.length = length;
+            this.width = width;
+            efficient = eff;
+        }
+
+        //Properties
+        //allow read/write access to private data members
+        //public, use same data type as what you're referencing, named same as what you're referencing, just capitalized
+
+        public bool Efficient  //bouncer that guards private data -- controls access to private data member data initialized by constructor methods above
+        {
+            get //describes how to read from the property
+            {
+                return efficient;
+
+            }
+
+            set //describes how to write to the property
+            {
+                efficient = value;  //must be true or false because bool
+                //user sets to true or false & it's unprotected
+            }
+        }
+
+
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+
+            set
+            { //protected length -- must be greater than or equal to 4 in
+                if (value>=4)
+                { length = value; }
+                else
+                { Console.WriteLine("Invalid length");
+
+                    length = 24;
+                }
+
+                //could use while loop instead of if else
+            }
+        }
+
+        public int Width
+        {
+            get
+            { return width; }
+            set
+            {
+                if (value >= 4)
+                { width = value; }
+                else
+                { Console.WriteLine("Invalid width");
+                    width = 12;
+                }
+            }
+
+        }
+            //Methods
+        //implement actions or behaviors of window
+
+        //calculate price
+        // price = 2* (length + height)
+        //if (eff) $75
+
+        //each window can calculate its own price based on its values -- doesn't need into passed into method here
+
+        public double GetPrice()
+        {
+            double price;
+            price = 2 * (length + width) * 2.50;
+
+            //data for length and width come from class
+
+            if (efficient== true)
+            { price = price + 75;
+            }
+
+            return price;
+        }
+
+    }
+}
